90dfd1ec6b8bb076318b4d090fa75fa47ecb3edb
Michel Alves <micheldejesus@gmail.com>
2024-07-31 19:22:53 -0300

    change tab onMouseUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { GlobalService } from './service/global.service';
 import { Languages } from './pages/settings/settings.component.model';
@@ -15,7 +15,10 @@ export class AppComponent {
   title = 'Portfolio';
   isDarkTheme = false;
   language = 'En';
+  tabs = ['', 'projects', 'settings']
   selectedTab = '';
+  mousePositionStart = 0;
+  mousePositionEnd = 0;
 
   constructor(private router: Router, private globalService: GlobalService) {
     this.globalService.switchColorSchema.subscribe({
@@ -29,5 +32,27 @@ export class AppComponent {
   changeTab(tab: string) {
     this.selectedTab = tab;
     this.router.navigateByUrl(tab);
+  }
+
+  @HostListener('window:touchstart', ['$event'])
+  onTouchStartEvent(event: TouchEvent) {
+    console.log('onTouchStartEvent event', event);
+    this.mousePositionStart = event.touches[0].clientX;
+  }
+
+  @HostListener('window:touchend', ['$event'])
+  onTouchEndEvent(event: TouchEvent) {
+    
+    this.mousePositionEnd = event.changedTouches[0].clientX;
+    const selectedTabIndex = this.tabs.indexOf(this.selectedTab);
+    const isDrag = Math.abs(Math.abs(this.mousePositionStart) - Math.abs(this.mousePositionEnd)) > 100;
+    if (this.mousePositionStart > this.mousePositionEnd && isDrag) {
+      console.log('onTouchEndEvent right', event);
+      if (selectedTabIndex < (this.tabs.length-1)) this.selectedTab = this.tabs[selectedTabIndex+1];
+      this.router.navigateByUrl(this.selectedTab);
+    } else if (this.mousePositionStart < this.mousePositionEnd && isDrag) {
+      if (selectedTabIndex > 0) this.selectedTab = this.tabs[selectedTabIndex-1];
+      this.router.navigateByUrl(this.selectedTab);
+    }
   }
 }
