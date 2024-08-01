833c889507a45cbdd6b559d58fb9d5355432ffab
Michel Alves <micheldejesus@gmail.com>
2024-08-01 19:26:10 -0300

    css enhancements

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -11,13 +11,11 @@ import { Languages, Settings, defaultSettings } from './settings.component.model
 })
 export class SettingsComponent {
   settings: Settings = defaultSettings;
-  formattedTheme = 'Light';
   
   constructor(private globalService: GlobalService) {}
 
   changeColor() {
     this.settings.theme = this.settings.theme === 'light' ? 'dark' : 'light';
-    this.formattedTheme = this.settings.theme[0].toUpperCase() + this.settings.theme.slice(1);
     this.globalService.switchColorSchema.next({
       ...this.settings
     })
